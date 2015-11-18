8210a493684845d54ae5c85aac8dc96b7bf08305
golonikum <goloniko@gmail.com>
2015-11-18 17:29:45 +0300

    Some changes in daily.js

--- scripts/daily.js
+++ scripts/daily.js
@@ -14,28 +14,32 @@ model.getAllSortedEvents(true, function( err, events ) {
         return day.isBefore( plusWeek );
     } );
 
-    var html = '<ul style="font-size:16px;">';
-    events.forEach(function( e ) {
-        var color = ( e.date === todayStr ? 'color:red;' : ( e.date === tomorrowStr ? 'color:#ef7808;' : '' ) ),
-            bold = ' style="font-weight:bold"';
-        html += format(
-            '<li style="line-height:30px;{0}"><span>{1} {2}</span>&nbsp;<span{3}>{4}</span>&nbsp;<span{5}>{6}</span></li>',
-            color,
-            e.date, e.dayOfWeek,
-            e.isPerson ? '' : bold, e.event,
-            e.isPerson ? bold : '', e.host
-        );
-    });
-    html += '</ul>';
+    if ( events.length > 0 ) {
+        var html = '<ul style="font-size:16px;">';
+        events.forEach(function( e ) {
+            var color = ( e.date === todayStr ? 'color:red;' : ( e.date === tomorrowStr ? 'color:#ef7808;' : '' ) ),
+                bold = ' style="font-weight:bold"';
+            html += format(
+                '<li style="line-height:30px;{0}"><span>{1} {2}</span>&nbsp;<span{3}>{4}</span>&nbsp;<span{5}>{6}</span></li>',
+                color,
+                e.date, e.dayOfWeek,
+                e.isPerson ? '' : bold, e.event,
+                e.isPerson ? bold : '', e.host
+            );
+        });
+        html += '</ul>';
 
-    var sendgrid  = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    sendgrid.send({
-        to: 'goloniko@gmail.com',
-        from: 'contacts2@golonikum.net',
-        subject: '🎂 Ближайшие события',
-        html: html
-    }, function(err, json) {
-        if (err) { return console.log(err); }
-        else { return console.log('Email was successfully send.'); }
-    });
+        var sendgrid  = require('sendgrid')(process.env.SENDGRID_API_KEY);
+        sendgrid.send({
+            to: 'goloniko@gmail.com',
+            from: 'contacts2@golonikum.net',
+            subject: '🎂 Ближайшие события',
+            html: html
+        }, function(err, json) {
+            if (err) { return console.log(err); }
+            else { return console.log('Email was successfully sent.'); }
+        });
+    } else {
+        console.log('There is nothing to send.');
+    }
 });
