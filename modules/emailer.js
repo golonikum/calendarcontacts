b9deb45abfd897e452801879b0cee7b49772e31c
Nikita Golov <nikgol@spb.tender.pro>
2016-08-03 19:22:24 +0300

    Show message after update person

--- modules/emailer.js
+++ modules/emailer.js
@@ -2,30 +2,32 @@ var postgres = require('./postgres');
 var emailer = require('./emailer');
 var moment = require('moment');
 
-module.exports = {
-	send: function( cfg, cb ) {
-		var sendgrid  = require('sendgrid')(process.env.SENDGRID_API_KEY);
+function sendEmail( cfg, cb ) {
+	var sendgrid  = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
-		sendgrid.send({
-			to: 'goloniko@gmail.com',
-			from: 'contacts2@golonikum.net',
-			subject: cfg.subject || '🎂 Ближайшие события',
-			html: cfg.html || 'Авторассылка',
-			files: cfg.files || undefined
-		}, function(err) {
-			if (err) {
-				return console.log( err );
-			} else {
-				cb && cb();
-				return console.log('Email was successfully sent.');
-			}
-		});
-	},
+	sendgrid.send({
+		to: 'goloniko@gmail.com',
+		from: 'contacts2@golonikum.net',
+		subject: cfg.subject || '🎂 Ближайшие события',
+		html: cfg.html || 'Авторассылка',
+		files: cfg.files || undefined
+	}, function(err) {
+		if (err) {
+			return console.log( err );
+		} else {
+			cb && cb();
+			return console.log('Email was successfully sent.');
+		}
+	});
+}
+
+module.exports = {
+	send: sendEmail,
 
 	sendBackupPersons: function( cfg, cb ) {
 		postgres.getPersonsJson(function(err, persons) {
 			if (!err) {
-				emailer.send({
+				sendEmail({
 					html: cfg.html || undefined,
 					subject: '🎂 Backup',
 					files: [{filename: 'persons.' + moment().format('YYYY-MM-DD-HHmmss') + '.json', content: JSON.stringify(persons)}]
