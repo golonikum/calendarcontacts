ff06e314962fdd812d7d00408b666a131347885e
Nikita Golov <nikgol@spb.tender.pro>
2016-08-03 18:13:31 +0300

    Update Backup title

--- modules/routes.js
+++ modules/routes.js
@@ -78,7 +78,7 @@ module.exports = {
 		        postgres.getPersonsJson(function(err, persons) {
 		            if (!err) {
 			            emailer.send({
-				            subject: 'Backup',
+				            subject: '🎂 Backup',
 				            files: [{filename: 'persons.js', content: JSON.stringify(persons)}]
 			            }, function(){
 				            res.redirect('/person?id=' + id);
